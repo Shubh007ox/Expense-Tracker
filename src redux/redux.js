9da8604aec13207fbf8c6050cb37c1e507506fbf
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2023-01-12 05:13:33 -0800

    Add files via upload

--- src redux/redux.js
+++ src redux/redux.js
@@ -3,13 +3,18 @@ const redux = require('redux');
 function counterReducer(state={counter : 0},action) {
     if(action.type === 'decrement'){
         return {counter : state.counter - 1}
-    }else{
+    }else if(action.type === 'increment2'){
+        return {counter : state.counter + 2}
+    }else if(action.type === 'decrement2'){
+        return {counter : state.counter - 2}
+    }
+    else{
         return {counter : state.counter + 1}
     }
     // return {
     //     counter : state.counter + 1
     // }
-
+    return state;
 }
 
 const store = redux.createStore(counterReducer);
@@ -27,4 +32,7 @@ store.dispatch({type : 'increment'});
 store.dispatch({type : 'increment'});
 store.dispatch({type : 'increment'});
 store.dispatch({type : 'increment'});
+store.dispatch({type : 'decrement'});
+store.dispatch({type : 'increment2'});
+store.dispatch({type : 'decrement2'});
 store.dispatch({type : 'decrement'});
